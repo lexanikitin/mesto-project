6a2636147d3be4da5a260495604945282e40b264
Alexey Nikitin <lexa.nikitin@gmail.com>
2022-07-22 13:08:33 +0300

    Редактирование содержимого страницы через модальное окно

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,36 @@
-const profileCloseBtn = document.querySelector('.popup__close');
+const profileName = document.querySelector('.profile__name');
+const profileSubtitle = document.querySelector('.profile__subtitle');
 const profileOpenBtn = document.querySelector('.profile__edit-button');
+
 const profilePopup = document.querySelector('.popup-profile');
+const profilePopupName = document.querySelector('#name');
+const profilePopupSubtitle = document.querySelector('#subtitle');
+const profileCloseBtn = document.querySelector('.popup__close');
+const profileFormElement = profilePopup.querySelector('.form');
+
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
 }
+
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
 
-profileOpenBtn.addEventListener('click', ()=>{
+function formProfileSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = profilePopupName.value;
+  profileSubtitle.textContent = profilePopupSubtitle.value;
+  closePopup(profilePopup);
+}
+
+profileOpenBtn.addEventListener('click', () => {
   openPopup(profilePopup);
+  profilePopupName.value = profileName.textContent;
+  profilePopupSubtitle.value = profileSubtitle.textContent;
 });
-profileCloseBtn.addEventListener('click', ()=>{
+profileCloseBtn.addEventListener('click', () => {
   closePopup(profilePopup);
 });
+
+profileFormElement.addEventListener('submit', formProfileSubmitHandler);
