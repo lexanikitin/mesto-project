b182d4863ed5a0090d3776df2a46fcc5f32689f4
Alexey Nikitin <lexa.nikitin@gmail.com>
2022-10-30 16:14:57 +0300

    bugfix: обработчики закрытия модальных окон перенесены в конструктор, исключено дублирование слушателей

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,9 +1,20 @@
 export default class Popup {
   constructor(selector) {
     this._popupElement = document.querySelector(selector)
+    this._handleEscClose = (evt) => {
+      if (evt.key === 'Escape') {
+        this.close();
+      }
+    }
+    this._handleOverlay = (evt) => {
+      if (evt.target.classList.contains('popup')) {
+        this.close();
+      }
+    }
+    this._handleClsButton = () => {
+      this.close();
+    }
   }
-
-
 
   // метод открытия модального окна
   open() {
@@ -18,27 +29,14 @@ export default class Popup {
   }
 
   setEventListeners() {
-    this._popupElement.addEventListener('mousedown', this._handleOverlay.bind(this));
-    document.addEventListener('keydown', (evt) => {
-      this._handleEscClose(evt);
-    });
-    this._popupElement.querySelector('.popup__close').addEventListener('click', this.close.bind(this));
+    this._popupElement.addEventListener('mousedown', this._handleOverlay);
+    document.addEventListener('keydown', this._handleEscClose);
+    this._popupElement.querySelector('.popup__close').addEventListener('click', this._handleClsButton);
   }
 
   removeEventListeners() {
     this._popupElement.removeEventListener('mousedown', this._handleOverlay);
+    this._popupElement.querySelector('.popup__close').removeEventListener('click', this._handleClsButton);
     document.removeEventListener('keydown', this._handleEscClose);
-  }
-
-  _handleEscClose(evt) {
-    if (evt.key === 'Escape') {
-      this.close();
-    }
-  }
-
-  _handleOverlay(evt) {
-    if (evt.target.classList.contains('popup')) {
-      this.close();
-    }
   }
 }
